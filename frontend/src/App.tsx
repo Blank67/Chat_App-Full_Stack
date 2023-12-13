d3685a8502dc59006d5575f20014cfafe606c75c
Blank67 <hmnshshrm67@gmail.com>
2023-12-13 13:39:22 +0530

    frontend part 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/1",
+        element: <div>Hello1</div>,
+    },
+]);
+
 function App() {
-    return <div>Hello</div>;
+    return (
+        <div className="root-div">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
 export default App;

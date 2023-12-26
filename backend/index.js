4c6c314e6d13f27459bb6a50011e2d34358f9839
Blank67 <hmnshshrm67@gmail.com>
2023-12-26 14:38:24 +0530

    backend part 5

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,56 @@ server.use("/api/message", messageRoutes);
 server.use(notFound);
 server.use(errorHandler);
 
+//SOCKET IO
+const socketIO = require("socket.io");
+
 connectDB().then(() => {
-    server.listen(process.env.PORT, () => {
+    const activeServer = server.listen(process.env.PORT, () => {
         console.log("SERVER STARTED!");
+    });
+
+    //CONNECTING SOCKET IO
+    const socketSetting = {
+        pingTimeout: 60000,
+        cors: {
+            origin: process.env.CLIENT_ORIGIN,
+        },
+    };
+    const socket = socketIO(activeServer, socketSetting);
+    socket.on("connection", (skt) => {
+        console.log("CONNECTED TO SOCKET.IO!");
+        skt.on("setup", (userId) => {
+            console.log("User:", userId);
+            skt.join(userId);
+            skt.emit("connected");
+        });
+        skt.on("join_chat", (room) => {
+            console.log("User joined room:", room);
+            skt.join(room);
+        });
+        skt.on("new_message", (newMessageReceived) => {
+            const chat = newMessageReceived.chat;
+            if (!chat.users) {
+                console.log("chat.users not defined!");
+                return;
+            }
+            chat.users.forEach((user) => {
+                if (user._id === newMessageReceived.sender._id) {
+                    return;
+                }
+                console.log(newMessageReceived);
+                skt.in(user._id).emit("message_received", newMessageReceived);
+            });
+        });
+        skt.on("typing", (room) => {
+            skt.in(room).emit("typing");
+        });
+        skt.on("stop_typing", (room) => {
+            skt.in(room).emit("stop_typing");
+        });
+        skt.off("setup", () => {
+            console.log("USER DISCONNECTED. CLOSING SOCKET!");
+            skt.leave(userData._id);
+        });
     });
 });

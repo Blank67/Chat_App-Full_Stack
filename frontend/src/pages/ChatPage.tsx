ff002217572a54d8044ca38b7c23566fe982f289
Blank67 <hmnshshrm67@gmail.com>
2024-02-12 15:28:59 +0530

    env

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import SideDrawer from "../components/sideDrawer/SideDrawer";
 import MyChats from "../components/myChats/MyChats";
-import ChatBox from "../components/chatbox/Chatbox";
+import ChatBox from "../components/chatBox/ChatBox";
 
 const ChatPage = () => {
     return (
